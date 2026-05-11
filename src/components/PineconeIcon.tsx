7c2702d9f17c2cb4d85f5d7c038f09ed0b07b015
Ben Scheiner <brmscheiner@gmail.com>
2026-05-10 23:44:49 -0700

    New pinecone icon

--- src/components/PineconeIcon.tsx
+++ src/components/PineconeIcon.tsx
@@ -10,79 +10,49 @@ export function PineconeIcon(props: SVGProps<SVGSVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <rect x="7" y="20" width="1" height="3" />
-      <rect x="10" y="21" width="1" height="2" />
-      <rect x="12" y="20" width="1" height="2" transform="rotate(90 12 20)" />
-      <rect x="10" y="22" width="1" height="2" transform="rotate(90 10 22)" />
-      <rect x="15" y="20" width="1" height="3" transform="rotate(90 15 20)" />
-      <rect x="16" y="19" width="1" height="2" transform="rotate(90 16 19)" />
-      <rect x="13" y="16" width="1" height="2" transform="rotate(90 13 16)" />
-      <rect x="12" y="17" width="1" height="3" transform="rotate(90 12 17)" />
-      <rect x="14" y="10" width="1" height="2" transform="rotate(90 14 10)" />
-      <rect x="17" y="11" width="1" height="2" transform="rotate(90 17 11)" />
-      <rect x="16" y="10" width="1" height="2" transform="rotate(90 16 10)" />
-      <rect x="18" y="10" width="1" height="2" transform="rotate(90 18 10)" />
-      <rect x="19" y="9" width="1" height="2" transform="rotate(90 19 9)" />
-      <rect x="15" y="15" width="1" height="2" transform="rotate(90 15 15)" />
-      <rect x="17" y="15" width="1" height="2" transform="rotate(90 17 15)" />
-      <rect x="16" y="18" width="1" height="2" />
-      <rect x="17" y="17" width="1" height="2" />
-      <rect x="16" y="16" width="1" height="2" />
-      <rect x="18" y="14" width="1" height="2" transform="rotate(90 18 14)" />
-      <rect x="18" y="16" width="1" height="2" />
-      <rect x="18" y="14" width="1" height="2" />
-      <rect x="19" y="13" width="1" height="2" />
-      <rect x="19" y="11" width="1" height="2" />
-      <rect x="20" y="8" width="1" height="4" />
-      <rect x="19" y="7" width="1" height="2" />
-      <rect x="18" y="6" width="1" height="2" />
-      <rect x="18" y="3" width="1" height="2" />
-      <rect x="19" y="4" width="1" height="3" />
-      <rect x="17" y="2" width="1" height="2" />
-      <rect x="13" y="8" width="1" height="2" />
-      <rect x="11" y="11" width="1" height="2" />
-      <rect x="12" y="12" width="1" height="2" />
-      <rect x="12" y="11" width="1" height="2" />
-      <rect x="12" y="14" width="1" height="2" />
-      <rect x="11" y="18" width="1" height="2" />
-      <rect x="7" y="17" width="1" height="3" />
-      <rect x="7" y="15" width="1" height="2" />
-      <rect x="6" y="14" width="1" height="2" />
-      <rect x="17" y="12" width="1" height="2" />
-      <rect x="16" y="11" width="1" height="2" />
-      <rect x="19" y="9" width="1" height="1" />
-      <rect x="15" y="4" width="1" height="2" />
-      <rect x="14" y="3" width="1" height="2" />
-      <rect x="15" y="2" width="1" height="2" transform="rotate(-90 15 2)" />
-      <rect x="12" y="2" width="1" height="3" transform="rotate(-90 12 2)" />
-      <rect x="10" y="3" width="1" height="3" transform="rotate(-90 10 3)" />
-      <rect x="12" y="4" width="1" height="2" transform="rotate(-90 12 4)" />
-      <rect x="9" y="4" width="1" height="2" transform="rotate(-90 9 4)" />
-      <rect x="8" y="5" width="1" height="2" transform="rotate(-90 8 5)" />
-      <rect x="7" y="6" width="1" height="2" transform="rotate(-90 7 6)" />
-      <rect x="6" y="7" width="1" height="2" transform="rotate(-90 6 7)" />
-      <rect x="5" y="20" width="1" height="3" transform="rotate(-90 5 20)" />
-      <rect x="8" y="20" width="1" height="3" transform="rotate(-90 8 20)" />
-      <rect x="8" y="17" width="1" height="2" transform="rotate(-90 8 17)" />
-      <rect x="9" y="13" width="1" height="2" transform="rotate(-90 9 13)" />
-      <rect x="5" y="14" width="1" height="2" transform="rotate(-90 5 14)" />
-      <rect x="11" y="7" width="1" height="2" transform="rotate(-90 11 7)" />
-      <rect x="12" y="8" width="1" height="2" transform="rotate(-90 12 8)" />
-      <rect x="14" y="7" width="1" height="2" transform="rotate(-90 14 7)" />
-      <rect x="16" y="7" width="1" height="2" transform="rotate(-90 16 7)" />
-      <rect x="7" y="9" width="1" height="2" transform="rotate(-90 7 9)" />
-      <rect x="7" y="9" width="1" height="2" transform="rotate(-180 7 9)" />
-      <rect x="6" y="11" width="1" height="3" transform="rotate(-180 6 11)" />
-      <rect x="8" y="14" width="1" height="2" transform="rotate(-180 8 14)" />
-      <rect x="9" y="13" width="1" height="2" transform="rotate(-180 9 13)" />
-      <rect x="9" y="11" width="1" height="2" transform="rotate(-180 9 11)" />
-      <rect x="10" y="9" width="1" height="2" transform="rotate(-180 10 9)" />
-      <rect x="11" y="8" width="1" height="2" transform="rotate(-180 11 8)" />
-      <rect x="11" y="6" width="1" height="2" transform="rotate(-180 11 6)" />
-      <rect x="5" y="13" width="1" height="3" transform="rotate(-180 5 13)" />
-      <rect x="5" y="15" width="1" height="2" transform="rotate(-180 5 15)" />
-      <rect x="5" y="18" width="1" height="3" transform="rotate(-180 5 18)" />
-      <rect x="6" y="19" width="1" height="2" transform="rotate(-180 6 19)" />
+      <rect x="9" y="2" width="6" height="1" />
+      <rect x="7" y="3" width="10" height="1" />
+      <rect x="6" y="4" width="4" height="1" />
+      <rect x="14" y="4" width="4" height="1" />
+      <rect x="6" y="5" width="4" height="1" />
+      <rect x="16" y="5" width="2" height="1" />
+      <rect x="6" y="6" width="2" height="1" />
+      <rect x="9" y="6" width="3" height="1" />
+      <rect x="16" y="6" width="2" height="1" />
+      <rect x="5" y="7" width="3" height="1" />
+      <rect x="9" y="7" width="4" height="1" />
+      <rect x="16" y="7" width="3" height="1" />
+      <rect x="5" y="8" width="2" height="1" />
+      <rect x="11" y="8" width="2" height="1" />
+      <rect x="15" y="8" width="4" height="1" />
+      <rect x="5" y="9" width="2" height="1" />
+      <rect x="12" y="9" width="7" height="1" />
+      <rect x="4" y="10" width="3" height="1" />
+      <rect x="12" y="10" width="3" height="1" />
+      <rect x="17" y="10" width="3" height="1" />
+      <rect x="4" y="11" width="6" height="1" />
+      <rect x="11" y="11" width="3" height="1" />
+      <rect x="17" y="11" width="3" height="1" />
+      <rect x="4" y="12" width="9" height="1" />
+      <rect x="18" y="12" width="2" height="1" />
+      <rect x="4" y="13" width="2" height="1" />
+      <rect x="11" y="13" width="2" height="1" />
+      <rect x="18" y="13" width="2" height="1" />
+      <rect x="4" y="14" width="2" height="1" />
+      <rect x="12" y="14" width="2" height="1" />
+      <rect x="17" y="14" width="3" height="1" />
+      <rect x="4" y="15" width="3" height="1" />
+      <rect x="12" y="15" width="2" height="1" />
+      <rect x="17" y="15" width="3" height="1" />
+      <rect x="5" y="16" width="3" height="1" />
+      <rect x="13" y="16" width="2" height="1" />
+      <rect x="16" y="16" width="3" height="1" />
+      <rect x="6" y="17" width="5" height="1" />
+      <rect x="13" y="17" width="5" height="1" />
+      <rect x="7" y="18" width="10" height="1" />
+      <rect x="10" y="19" width="4" height="1" />
+      <rect x="10" y="20" width="4" height="1" />
+      <rect x="10" y="21" width="4" height="1" />
     </svg>
   );
 }
